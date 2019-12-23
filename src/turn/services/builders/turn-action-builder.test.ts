74f8e97b12660f90d22b897bc98cf3dbde97a2de
Ryan Saunders <rhyeen@gmail.com>
2019-12-23 15:15:38 -0700

    adding turn action builder

---
+++ src/turn/services/builders/turn-action-builder.test.ts
@@ -0,0 +1,32 @@
+import { TurnActionBuilder } from './turn-action-builder';
+import { PlaceMinionAction } from '../../entities/turn-action/player-turn-actions/place-minion-action';
+import { PlayMinionAttackAction } from '../../entities/turn-action/player-turn-actions/play-minion-attack-action';
+import { OpponentMinionActionTarget } from '../../entities/action-target';
+
+describe('buildTurnActions', () => {
+  test('build place minion and attack with minion action', () => {
+    const turnActions = [
+      {"type":"placeMinion","source":{"handIndex":1},"target":{"fieldIndex":1}},
+      {"type":"playMinionAttack","source":{"fieldIndex":1},"targets":[{"type":"targetOpponentMinion","fieldIndex":1}]}
+    ];
+    const result = TurnActionBuilder.buildTurnActions(turnActions);
+    expect(result.length).toBe(2);
+    const resultA = result[0];
+    const resultB = result[1];
+    expect(resultA instanceof PlaceMinionAction).toBeTruthy();
+    expect(resultB instanceof PlayMinionAttackAction).toBeTruthy();
+    if (!(resultA instanceof PlaceMinionAction) || !(resultB instanceof PlayMinionAttackAction)) {
+      return;
+    }
+    expect(resultA.playerSourceHandIndex).toBe(1);
+    expect(resultA.playerTargetFieldIndex).toBe(1);
+    expect(resultB.playerSourceFieldIndex).toBe(1);
+    expect(resultB.targets.length).toBe(1);
+    const resultBtargetA = resultB.targets[0];
+    expect(resultBtargetA instanceof OpponentMinionActionTarget).toBeTruthy();
+    if (!(resultBtargetA instanceof OpponentMinionActionTarget)) {
+      return;
+    }
+    expect(resultBtargetA.targetOpponentFieldIndex).toBe(1);
+  });
+});
---
+++ src/turn/services/builders/turn-action-builder.ts
@@ -0,0 +1,68 @@
+import { TurnAction } from "../../entities/turn-action/turn-action";
+import { PlaceMinionAction } from "../../entities/turn-action/player-turn-actions/place-minion-action";
+import { ActionType, ActionTargetType } from "../../enums/action-type";
+import { PlayMinionAttackAction } from "../../entities/turn-action/player-turn-actions/play-minion-attack-action";
+import { ActionTarget, OpponentActionTarget, OpponentMinionActionTarget, PlayerActionTarget, PlayerMinionActionTarget } from "../../entities/action-target";
+import { PlayMinionAbilityAction } from "../../entities/turn-action/player-turn-actions/play-minion-ability-action";
+import { PlaySpellAbilityAction } from "../../entities/turn-action/player-turn-actions/play-spell-ability-action";
+import { CraftBaseCardAction } from "../../entities/turn-action/player-turn-actions/craft-base-card-action";
+import { AddCraftingPartAction } from "../../entities/turn-action/player-turn-actions/add-crafting-part-action";
+import { AddCraftedCardToDeckAction } from "../../entities/turn-action/player-turn-actions/add-crafted-card-to-deck-action";
+
+export class TurnActionBuilder {
+    static buildTurnActions(turnActionsData: any[]):TurnAction[] {
+        const turnActions = [];
+        for (const turnActionData of turnActionsData) {
+            turnActions.push(TurnActionBuilder.buildTurnAction(turnActionData));
+        }
+        return turnActions;
+    }
+
+    static buildTurnAction(turnActionData: any):TurnAction {
+        const targets = TurnActionBuilder.buildActionTargets(turnActionData.targets);
+        switch (turnActionData.type) {
+            case ActionType.PlaceMinion:
+                return new PlaceMinionAction(turnActionData.source.handIndex, turnActionData.target.fieldIndex);
+            case ActionType.PlayMinionAttack:
+                return new PlayMinionAttackAction(turnActionData.source.fieldIndex, targets);
+            case ActionType.PlayMinionAbility:
+                return new PlayMinionAbilityAction(turnActionData.source.fieldIndex, targets);
+            case ActionType.PlaySpellAbility:
+                return new PlaySpellAbilityAction(turnActionData.source.handIndex, targets);
+            case ActionType.CraftBaseCard:
+                return new CraftBaseCardAction(turnActionData.forgeSlotIndex);
+            case ActionType.AddCraftingPart:
+                return new AddCraftingPartAction(turnActionData.craftingPartIndex, turnActionData.forgeSlotIndex);
+            case ActionType.AddCraftedCardToDeck:
+                return new AddCraftedCardToDeckAction(turnActionData.forgeSlotIndex, turnActionData.numberOfInstances, turnActionData.cardId, turnActionData.cardName);
+            default:
+                throw new Error(`unexpected turn action type: ${turnActionData.type}`);
+        }
+    }
+
+    private static buildActionTargets(actionTargetsData: any[]):ActionTarget[] {
+        if (!actionTargetsData) {
+            return null;
+        }
+        const actionTargets = [];
+        for (const actionTargetData of actionTargetsData) {
+            actionTargets.push(TurnActionBuilder.buildActionTarget(actionTargetData));
+        }
+        return actionTargets;
+    }
+
+    private static buildActionTarget(actionTargetData: any):ActionTarget {
+        switch (actionTargetData.type) {
+            case ActionTargetType.TargetOponnet:
+                return new OpponentActionTarget();
+            case ActionTargetType.TargetOpponentMinion:
+                return new OpponentMinionActionTarget(actionTargetData.fieldIndex);
+            case ActionTargetType.TargetPlayer:
+                return new PlayerActionTarget();
+            case ActionTargetType.TargetPlayerMinion:
+                return new PlayerMinionActionTarget(actionTargetData.fieldIndex);
+            default:
+                throw new Error(`unexpected action target type: ${actionTargetData.type}`);
+        }
+    }
+}
